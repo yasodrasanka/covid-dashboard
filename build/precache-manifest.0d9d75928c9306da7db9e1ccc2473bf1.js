e19b64ec387e52872b2e4e0605c6cf1c3f844969
Yasod <yasodrasanka@gmail.com>
2020-04-14 14:07:08 +0530

    build

---
+++ build/precache-manifest.0d9d75928c9306da7db9e1ccc2473bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf655cba6b0a3a5698f94833a724f7ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33298296d126cbdec76a",
+    "url": "/static/css/main.8c960495.chunk.css"
+  },
+  {
+    "revision": "21a87a01e11af95f654b",
+    "url": "/static/js/2.aabeedca.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.aabeedca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33298296d126cbdec76a",
+    "url": "/static/js/main.a1f88f84.chunk.js"
+  },
+  {
+    "revision": "840a910121881ee012ae",
+    "url": "/static/js/runtime-main.d1bbc7a9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/covid.d7265326.png"
+  }
+]);
